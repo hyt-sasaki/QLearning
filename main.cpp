6f6bae74401c571206d3725d37c03a517884cc5a
Sasaki Hayato <h.sasaki.ynu@gmail.com>
2015-04-17 10:15:18 +0900

    Agentオブジェクトの初期化方法を修正
    Conflicts:
    	main.cpp

--- main.cpp
+++ main.cpp
@@ -36,9 +36,11 @@ int main(int argc, char const* argv[])
     for (unsigned int i = 1; i <= split; i++) {
         epsilon[i - 1] = 1.0 * (double)(split - i) / (double)split;
     }
+    const double alpha = 0.1;
+    const double gamma = 0.9;
 
     Environment env(ifs);
-    Agent agent = Agent();
+    Agent agent = Agent(alpha, gamma);
     while (episode <= episodeLimit) {
         unsigned int step = 0;
         cout << "************************" << endl;
